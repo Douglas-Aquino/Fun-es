f9155d6646d54525f1925dbae4fa55cb431f3e61
Douglas <douglinhauniverso@gmail.com>
2022-01-21 13:05:12 -0300

    Finalizado bb

---
+++ script.js
@@ -0,0 +1,32 @@
+//1 - crie uma função que exiba uma mensagem no console
+
+function frase (f){
+    console.log(`Voçê é ${f}`)
+}
+
+frase(`mais que um par de chifres.`)
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+function nome(nome){
+    console.log(`${nome}`)
+}
+nome(`Douglinhas.`)
+
+//3 - crie uma função que receba um nome, um número, e um estilo musical (parâmetros) e exiba no console
+
+function dados(N, NU ,EM){
+    console.log(`Meu nome é ${N},sou o número ${NU},e gosto de música ${EM}.`)
+}
+dados(`Douglas`,`1`,`Funk`)
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+
+function gosto(f,m){
+    console.log(`Eu gosto do filme ${f} e gosto da música ${m}.`  )
+}
+gosto(`Coraline`,`da xuxa ao contrário.`)
+
+/*Bônus*/
+
+//5 - crie uma função que retorne o triplo do número recebido no (parâmetro)
